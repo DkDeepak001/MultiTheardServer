465486c93c481141846f2ce5152ffbf1e37912b8
Nobody <nobody@example.com>
2021-01-16 07:47:01 -0800

    v1

--- client.py
+++ client.py
@@ -1,6 +1,9 @@
 import socket
 import sys
+import select as sel
 
+inp = sys.stdin
+socketList=[sys.stdin]
 def client_Server():
     if len(sys.argv) < 3:
         print("Usage: python3 {} hostname port".format(sys.argv[0]))
@@ -13,11 +16,35 @@ def client_Server():
 
     try:
         s.connect((host, port))
+
     except:
         print("Cannot reach the {}:{}...".format(host, port))
         sys.exit(-1)
 
-    print("connteced")
+    print("Connected to remote host. You can start sending messages...")
+    sys.stdout.write(">")
+    sys.stdout.flush()
+
+    while True:
+        read_ready, write_ready, error = sel.select(socketList, [], [])
+        # Block until connection is made
+        for sock in read_ready:
+            if sock == s:
+                data = data.recv(10)
+                data.decode()
+                if not data:
+                    print("Chat disconnected.")
+                    sys.exit()
+                else:
+                    sys.stdout.write(data)
+                    sys.stdout.write("> ")
+                    sys.stdout.flush()
+            else:
+                msg = sys.stdin.readline()
+                s.send(msg.encode())
+                sys.stdout.write("> ")
+                sys.stdout.flush()
+
 
 if __name__ == "__main__":
 	sys.exit(client_Server())
--- server.py
+++ server.py
@@ -13,14 +13,37 @@ def multi_Server():
     print("MultiThreadedChat is Listeing On " + str(port) + "....")
     while True:
             #Waiting For someone To Connect
-            ready_read,ready_write,error=sel.select(socketList,[],[],0)
-            for sock in ready_read:
+            read_ready, write_ready, error = sel.select(socketList, [], [])
+            for sock in read_ready:
                 if sock == s:
                     client_socket , addr = s.accept()
                     socketList.append(client_socket)
                     print("Client {}:{} connected.".format(addr[0], addr[1]))
+                    broadcast(s,client_socket, "{} entered our chatting room... \n".format(addr))
                 else:
-                    print("Connection Lost!")
-                    s.close()
+                    try:
+                        data = sock.recv(10)
+                        data =data.decode()
+                        if data:
+                            print(data)
+                        else:
+                            print("client {}: offline".format(addr[1]))
+                            if sock in socketList:
+                                socketList.remove(sock)
+                    except:
+                        print("client Except")
+                    continue
+
+def broadcast(server_socket, client_socket, message):
+    print("mssegae" + message)
+    for socket in socketList :
+        if socket != server_socket and socket != client_socket:
+            try:
+                socket.send(message.encode())
+            except:
+                socket.close()
+                if socket in socketList:
+                    socketList.remove(socket)
+
 if __name__ == "__main__":
     sys.exit(multi_Server())
